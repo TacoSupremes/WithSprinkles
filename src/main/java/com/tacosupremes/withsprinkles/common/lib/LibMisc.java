eb6d2db558bc256c49212e6cff35fef994b73714
TacoSupremes <zscharoun@gmail.com>
2017-06-24 18:12:20 -0400

    Version 1.02

--- src/main/java/com/tacosupremes/withsprinkles/common/lib/LibMisc.java
+++ src/main/java/com/tacosupremes/withsprinkles/common/lib/LibMisc.java
@@ -15,7 +15,7 @@ import net.minecraftforge.oredict.OreDictionary;
 public class LibMisc {
 	
 	public static final String MODID = "withsprinkles";
-	public static final String VERSION = "1.01";
+	public static final String VERSION = "1.02";
 	public static final String COMMONPROXY = "com.tacosupremes.withsprinkles.proxy.CommonProxy";
 	public static final String CLIENTPROXY = "com.tacosupremes.withsprinkles.proxy.ClientProxy";
 	
